6be70cd0b8ceb61223d73bb696da17a280437347
ethanc8 <echaroenpitaks@gmail.com>
2024-12-23 15:31:06 -0600

    Add libgnustep-gui

--- libgnustep-gui/build-gui.sh
+++ libgnustep-gui/build-gui.sh
@@ -5,10 +5,10 @@ source "$PREFIX/GNUstep/System/Library/Makefiles/GNUstep.sh"
 export CC="clang"
 export CXX="clang++"
 export LD="$(which ld.gold)"
-export LDFLAGS="-fuse-ld=$LD -ldispatch"
+export LDFLAGS="-fuse-ld=$LD $(gnustep-config --objc-libs)"
 
 # Build
 
-./configure --disable-newkvo --with-libiconv-library="$PREFIX/lib"
+./configure --enable-imagemagick
 make GNUSTEP_INSTALLATION_DOMAIN=SYSTEM debug=yes || exit 1
 make GNUSTEP_INSTALLATION_DOMAIN=SYSTEM install
