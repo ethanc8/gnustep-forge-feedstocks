0fe5d59b9860efaa249c3de1d99c05d90cc5925d
ethanc8 <echaroenpitaks@gmail.com>
2025-10-28 13:45:45 -0500

    [gnustep-libdispatch] lld, clean up build script

--- gnustep-libdispatch/build-libdispatch.sh
+++ gnustep-libdispatch/build-libdispatch.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-source "$PREFIX/GNUstep/System/Library/Makefiles/GNUstep.sh"
-
 export CC="clang"
 export CXX="clang++"
-export LD="$(which ld.gold)"
+export LD="$(which lld)"
 export LDFLAGS="-fuse-ld=$LD"
-
-# pre-create some directories
-mkdir -p "$PREFIX/GNUstep/Local/Library/Libraries"
-mkdir -p "$PREFIX/GNUstep/Local/Library/Headers"
 
 mkdir build
 cd build
@@ -20,14 +14,6 @@ extra_cmake_args=(
     -DCMAKE_ASM_COMPILER="${CC}"
     -DCMAKE_LINKER="${LD}"
     -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}"
-    # -DCMAKE_INSTALL_BINDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_TOOLS)"
-    # -DCMAKE_INSTALL_LIBDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_LIBRARIES)"
-    # -DCMAKE_INSTALL_INCLUDEDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_HEADERS)"
-    # -DCMAKE_INSTALL_OLDINCLUDEDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_HEADERS)"
-    # -DCMAKE_INSTALL_MANDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_DOC_MAN)"
-    # -DCMAKE_INSTALL_INFODIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_DOC_INFO)"
-    # -DCMAKE_INSTALL_DOCDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_DOC)"
-    # -DCMAKE_INSTALL_DATAROOTDIR="$(gnustep-config --variable=GNUSTEP_SYSTEM_LIBRARY)"
     -DINSTALL_PRIVATE_HEADERS=ON
     -DTESTS=OFF
 )
