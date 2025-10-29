30cac3210a6d03234fa47c49004696657a25ef55
ethanc8 <echaroenpitaks@gmail.com>
2025-10-28 19:27:05 -0500

    [libgnustep-boron] new package

---
+++ libgnustep-boron/build-boron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "$PREFIX/GNUstep/System/Library/Makefiles/GNUstep.sh"
+
+export CC="clang"
+export CXX="clang++"
+export LD="$(which ld.lld)"
+
+# Build
+
+make GNUSTEP_INSTALLATION_DOMAIN=SYSTEM debug=yes || exit 1
+make GNUSTEP_INSTALLATION_DOMAIN=SYSTEM install
